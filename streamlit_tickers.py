033a7643877bcaaa544b6d9fb954f5900575c756
Nor Rizal <dreameshuggah@yahoo.com>
2024-09-28 20:07:04 +0800

    Update streamlit_tickers.py

--- streamlit_tickers.py
+++ streamlit_tickers.py
@@ -209,8 +209,8 @@ with tab2:
     fig_scatter2 = px.scatter(recent_df2
                              , x="operatingMargins", y="returnOnEquity"
                              , color="shortName"
-                             #, size= 'forwardPE'
-                             #, symbol = 'market_trend'
+                             , size= 'forwardPE'
+                             , symbol = 'market_trend'
                              , hover_data=['ticker','market_trend','currentPrice','perc_Chg_52WkHigh']
                              )
     st.plotly_chart(fig_scatter2, key="ticker_b8", on_select="rerun")

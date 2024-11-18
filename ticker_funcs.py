65de26b59791ce43ff017e2fae830e61dfd4491a
Nor Rizal <dreameshuggah@yahoo.com>
2024-11-18 17:38:36 +0800

    Update ticker_funcs.py

--- ticker_funcs.py
+++ ticker_funcs.py
@@ -356,7 +356,7 @@ def fetchRecent(ticker_list,recent_ls):
     qry_recent_ratios = """
                     SELECT
                     -ROUND(  currentPrice/fiftyTwoWeekHigh  ,4)*100 AS perc_Chg_52WkHigh
-                    ,ROUND((targetMedianPrice/currentPrice)-1,4)*100 AS upside_perc_Chg_targetMedianPrice
+                    ,ROUND((targetMedianPrice/currentPrice)-1,4)*100 AS upside_Perc_targetMedianPrice
                     ,totalDebt/marketCap AS debt_ratio
                     ,*
                     FROM df 
